d75b670da0b01cc0c91b99c77b2a4f67fe928380
David St-Cyr <st-cyrdavid@hotmail.com>
2025-05-26 16:59:26 -0400

    first state in test game

--- test-library/Sources/Test.cpp
+++ test-library/Sources/Test.cpp
@@ -68,7 +68,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 #endif
         }
 
-        tGame.Start();
+        tGame.Start("MainMenu");
     }
 
     return 0;
